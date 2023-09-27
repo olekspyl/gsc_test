cf8d6c35f35c05710ef18b722cdde66b55eed1d7
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-09-27 13:44:14 +0200

    vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './shared/components/app/Header'
+import Footer from './shared/components/app/Footer'
+import Home from './pages/home/Home'
+
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
