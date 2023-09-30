45eb8e5e72f5ae62ec54a627df8e3677273e0cb8
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-09-30 16:31:18 +0200

    fix preload btn

--- src/shared/components/ui/ButtonPreorder.jsx
+++ src/shared/components/ui/ButtonPreorder.jsx
@@ -3,14 +3,23 @@ import leftSide from '../../../assets/icons/btn_side_left.svg';
 import trolley from '../../../assets/icons/trolley.svg';
 
 const ButtonPreorder = () => {
-    return (
-    <button className='hidden xl:flex xl:relative xl:items-center xl:bg-light xl:h-[39.5px]' type='button'>
-        <img className='hidden xl:flex xl:absolute xl:-left-10' src={leftSide} alt='left-side-btn'/>
-        <img className='hidden xl:flex xl:absolute xl:-left-8' src={trolley} alt='trolley'/>
-        <p className='hidden xl:flex xl:pl-2.5 xl:text-base xl:uppercase xl:font-medium xl:text-blackText font-oswald'>preorder now</p>
-        <img className='hidden xl:flex xl:absolute xl:left-[109px]' src={rightSide} alt='right-side-btn' />
-    </button>    
-  )
-}
+  return (
+    <button
+      className="hidden xl:flex xl:relative xl:items-center xl:bg-light xl:h-[39.5px]"
+      type="button"
+    >
+      <img className="hidden xl:flex xl:absolute xl:-left-10" src={leftSide} alt="left-side-btn" />
+      <img className="hidden xl:flex xl:absolute xl:-left-8" src={trolley} alt="trolley" />
+      <p className="hidden xl:flex xl:pl-2.5 xl:text-base xl:uppercase xl:font-medium xl:text-blackText font-oswald">
+        preorder now
+      </p>
+      <img
+        className="hidden xl:flex xl:absolute xl:left-[110px]"
+        src={rightSide}
+        alt="right-side-btn"
+      />
+    </button>
+  );
+};
 
 export default ButtonPreorder;
